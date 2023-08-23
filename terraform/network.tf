d39e2b2955270f01e296249beefc50f0b06dc78d
Владислав <106807250+Destian1995@users.noreply.github.com>
2023-08-23 15:20:28 +0400

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,12 +6,12 @@ resource "yandex_vpc_subnet" "subnet-stage" {
   name           = "subnet-stage"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["10.0.2.15/24"]
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 resource "yandex_vpc_subnet" "subnet-prod" {
   name           = "subnet-prod"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["10.0.2.15/24"]
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
