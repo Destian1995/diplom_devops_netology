f55437b0ca1be51719cb303373d4195ec839b0f2
Владислав <106807250+Destian1995@users.noreply.github.com>
2023-08-23 12:24:25 +0400

    Update main_install.sh

--- main_install.sh
+++ main_install.sh
@@ -64,14 +64,16 @@ echo "Ждем пока инфраструктура оживет..."
 sleep 120
 
 # Установка nerdctl на мастер-ноду
-#ssh ubuntu@$IP_MASTER "sudo sh -c 'curl -L https://github.com/containerd/nerdctl/releases/latest/download/nerdctl-$(uname -s)-$(uname -m) > /usr/local/bin/nerdctl'"
-#ssh ubuntu@$IP_MASTER "sudo chmod +x /usr/local/bin/nerdctl"
+ssh ubuntu@$IP_MASTER "yes | sudo sh -c 'curl -L https://github.com/containerd/nerdctl/releases/latest/download/nerdctl-$(uname -s)-$(uname -m) > /usr/local/bin/nerdctl'"
+ssh ubuntu@$IP_MASTER "sudo chmod +x /usr/local/bin/nerdctl"
 
 # Установка nerdctl на рабочие ноды
-#for worker_ip in $(terraform output -json external_ip_address_vm_instance_worker | jq -r '.[]'); do
-#    ssh ubuntu@$worker_ip "sudo sh -c 'curl -L https://github.com/containerd/nerdctl/releases/latest/download/nerdctl-$(uname -s)-$(uname -m) > /usr/local/bin/nerdctl'"
-#    ssh ubuntu@$worker_ip "sudo chmod +x /usr/local/bin/nerdctl"
-#done
+for worker_ip in $(terraform output -json external_ip_address_vm_instance_worker | jq -r '.[]'); do
+    ssh ubuntu@$worker_ip "yes | sudo sh -c 'curl -L https://github.com/containerd/nerdctl/releases/latest/download/nerdctl-$(uname -s)-$(uname -m) > /usr/local/bin/nerdctl'"
+    ssh ubuntu@$worker_ip "sudo chmod +x /usr/local/bin/nerdctl"
+done
+
+
 
 cd ../kubespray
 ansible-playbook -i ../kubespray/inventory/mycluster/hosts.ini ../kubespray/cluster.yml --become --ssh-common-args='-o StrictHostKeyChecking=no'
