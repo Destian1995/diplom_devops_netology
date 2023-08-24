4a9bb6760d236385ae21d384aa9bfd73e0983772
Владислав <106807250+Destian1995@users.noreply.github.com>
2023-08-24 10:53:19 +0400

    Update main_install.sh

--- main_install.sh
+++ main_install.sh
@@ -71,20 +71,6 @@ export IP_MASTER=$(terraform output -json external_ip_address_vm_instance_master
 echo "Ждем пока инфраструктура оживет..."
 sleep 120
 
-# Установка nerdctl на мастер-ноду
-#if ! ssh ubuntu@$IP_MASTER "[ -f /usr/local/bin/nerdctl ]"; then
-#    ssh ubuntu@$IP_MASTER "sudo sh -c 'curl -L https://github.com/containerd/nerdctl/releases/latest/download/nerdctl-\$(uname -s)-\$(uname -m) > /usr/local/bin/nerdctl'"
-#    ssh ubuntu@$IP_MASTER "sudo chmod +x /usr/local/bin/nerdctl"
-#fi
-
-# Установка nerdctl на рабочие ноды
-#for worker_ip in $(terraform output -json external_ip_address_vm_instance_worker | jq -r '.[]'); do
-#    if ! ssh ubuntu@$worker_ip "[ -f /usr/local/bin/nerdctl ]"; then
-#        ssh ubuntu@$worker_ip "sudo sh -c 'curl -L https://github.com/containerd/nerdctl/releases/latest/download/nerdctl-\$(uname -s)-\$(uname -m) > /usr/local/bin/nerdctl'"
-#        ssh ubuntu@$worker_ip "sudo chmod +x /usr/local/bin/nerdctl"
-#    fi
-#done
-
 cd ../kubespray
 ansible-playbook -i ../kubespray/inventory/mycluster/hosts.ini ../kubespray/cluster.yml --become --ssh-common-args='-o StrictHostKeyChecking=no'
 
